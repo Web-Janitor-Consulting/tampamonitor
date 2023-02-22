eb24f6b27e2abfc9e862b13cf65fb9250a435613
Michael Bishop <work@michaelbishop.me>
2023-02-21 23:38:58 -0500

    feat: add agenda pattern

---
+++ patterns/agenda-item.php
@@ -0,0 +1,32 @@
+<?php
+ /**
+  * Title: Agenda Item
+  * Slug: wjc/agenda-item
+  * Categories: featured, text
+  */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|30","right":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30"}}},"backgroundColor":"tertiary","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-tertiary-background-color has-background" style="padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:paragraph {"fontSize":"large"} -->
+<p class="has-large-font-size">#</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Youtube timestamp</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>text</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">(<em>Original motion initiated by  on )</em></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"fontSize":"large"} -->
+<h2 class="has-large-font-size">Notes</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Text</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
