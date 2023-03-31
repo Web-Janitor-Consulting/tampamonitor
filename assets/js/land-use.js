a26a3d859109629adffe6bf29975d511262fb66c
Michael Bishop <work@michaelbishop.me>
2023-03-30 22:11:12 -0400

    chore: update Current Zoning data

--- assets/js/land-use.js
+++ assets/js/land-use.js
@@ -18,7 +18,7 @@ map.on('load', function() {
             'id': 'current-zoning',
             'type': 'fill',
             'source': 'current-zoning',
-            "source-layer": "Zoning_District-cutts8",
+            "source-layer": "Zoning_District-2-57li4K",
             'layout': {
                 'visibility': 'visible'
             },
