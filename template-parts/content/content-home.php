22a3caf667f8c6604853ae798e43fabe85ca7c80
Michael Bishop <work@michaelbishop.me>
2023-04-01 12:40:45 -0400

    fix: use 300px post_thumbnail size for home page

--- template-parts/content/content-home.php
+++ template-parts/content/content-home.php
@@ -15,7 +15,7 @@
 	<header class="entry-header">
 		<figure class="post-thumbnail">
 			<a class="post-thumbnail-inner alignwide" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
-				<?php the_post_thumbnail( 'thumbnail' ); ?>
+				<?php the_post_thumbnail( 'medium' ); ?>
 			</a>
 			<?php if ( wp_get_attachment_caption( get_post_thumbnail_id() ) ) : ?>
 				<figcaption class="wp-caption-text"><?php echo wp_kses_post( wp_get_attachment_caption( get_post_thumbnail_id() ) ); ?></figcaption>
